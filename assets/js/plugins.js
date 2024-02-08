5268a4cdd3fc71d371c2a0bff42ec29604268b2f
ahmed abdellatif <mokaworld2023@gmail.com>
2024-02-08 02:10:17 +0200

    update

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,54 +1,24 @@
-// init owl carousel
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:false,
-//     autoplayHoverPause:true,
-//     lazyLoad:true,
-//     slideTransition: 'linear',
-//     responsiveClass:true,
-//     responsive:{
-//         0:{
-//             items:1,
-//             nav:true
-//         },
-//         600:{
-//             items:3,
-//             nav:false
-//         },
-//         1000:{
-//             items:5,
-//             nav:true,
-//             loop:false
-//         }
-//     }
-// })
 
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout: 2000,
-    autoplaySpeed: 2000,
-    autoplayHoverPause:true,
-    lazyLoad:true,
-    slideTransition: 'linear',
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
-
-
-
-
-
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplaySpeed: 2000,
+  autoplayHoverPause: true,
+  lazyLoad: true,
+  slideTransition: "linear",
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
