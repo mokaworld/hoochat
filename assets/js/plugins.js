7b2eca01b9d201ca8768d96c6ea440ad91fccd9f
ahmed abdellatif <mokaworld2023@gmail.com>
2024-02-10 00:20:25 +0200

    update sections

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,49 +1,76 @@
 // owl carowsel init
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  nav: false,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplaySpeed: 2000,
-  autoplayHoverPause: true,
-  lazyLoad: true,
-  slideTransition: "linear",
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 2,
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: true,
+    lazyLoad: true,
+    slideTransition: "linear",
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
     },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
-    },
-  },
+  });
 });
+
+  $("#owl-testi").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    navText: [
+      "<i <i class='fa-solid fa-arrow-left'></i>",
+      "<i <i class='fa-solid fa-arrow-right'></i>", 
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+
 
 // START SCROLL TOP
 let scrollTop = document.getElementById("scroll");
 
-this.addEventListener('click',function(){
+scrollTop.addEventListener("click", function () {
   scroll({
     top: 0,
     behavior: "smooth",
   });
 });
 
-this.addEventListener('scroll', function(){
+this.addEventListener("scroll", function () {
   if (scrollY >= 400) {
     scrollTop.style.display = "block";
   } else {
     scrollTop.style.display = "none";
   }
-  
 });
 // END SCROLL TOP
 
 let loading = document.getElementById("loading");
 
-this.addEventListener('load',function(){
+this.addEventListener("load", function () {
   loading.style.display = "none";
-})
+});
